7a062a9762a27dbaed3f2ac10ec3b02295e39ba5
Kizinho <adikekizinho@gmail.com>
2021-03-04 23:42:12 +0100

    update

--- database/seeds/UserCoinTableDataSeeder.php
+++ database/seeds/UserCoinTableDataSeeder.php
@@ -19,37 +19,37 @@ class UserCoinTableDataSeeder extends Seeder {
         $user_coin->earn = 2;
         $user_coin->save();
 
-        $user_coin2 = new UserCoin();
-        $user_coin2->user_id = 1;
-        $user_coin2->address = 'Ethudsdjhdjhdhjhhrhejhdjhje';
-        $user_coin2->coin_id = 2;
-        $user_coin2->amount = 10;
-        $user_coin2->earn = 2;
-        $user_coin2->save();
-
-        $user_coin3 = new UserCoin();
-        $user_coin3->user_id = 1;
-        $user_coin3->address = 'Ethudsdjhdjhdhjhhrhejhdjhje';
-        $user_coin3->coin_id = 3;
-        $user_coin3->amount = 10;
-        $user_coin3->earn = 2;
-        $user_coin3->save();
-
-        $user_coin4 = new UserCoin();
-        $user_coin4->user_id = 1;
-        $user_coin4->address = 'Ethudsdjhdjhdhjhhrhejhdjhje';
-        $user_coin4->coin_id = 4;
-        $user_coin4->amount = 10;
-        $user_coin4->earn = 2;
-        $user_coin4->save();
-
-        $user_coin5 = new UserCoin();
-        $user_coin5->user_id = 1;
-        $user_coin5->address = 'Ethudsdjhdjhdhjhhrhejhdjhje';
-        $user_coin5->coin_id = 5;
-        $user_coin5->amount = 10;
-        $user_coin5->earn = 2;
-        $user_coin5->save();
+//        $user_coin2 = new UserCoin();
+//        $user_coin2->user_id = 1;
+//        $user_coin2->address = 'Ethudsdjhdjhdhjhhrhejhdjhje';
+//        $user_coin2->coin_id = 2;
+//        $user_coin2->amount = 10;
+//        $user_coin2->earn = 2;
+//        $user_coin2->save();
+//
+//        $user_coin3 = new UserCoin();
+//        $user_coin3->user_id = 1;
+//        $user_coin3->address = 'Ethudsdjhdjhdhjhhrhejhdjhje';
+//        $user_coin3->coin_id = 3;
+//        $user_coin3->amount = 10;
+//        $user_coin3->earn = 2;
+//        $user_coin3->save();
+//
+//        $user_coin4 = new UserCoin();
+//        $user_coin4->user_id = 1;
+//        $user_coin4->address = 'Ethudsdjhdjhdhjhhrhejhdjhje';
+//        $user_coin4->coin_id = 4;
+//        $user_coin4->amount = 10;
+//        $user_coin4->earn = 2;
+//        $user_coin4->save();
+//
+//        $user_coin5 = new UserCoin();
+//        $user_coin5->user_id = 1;
+//        $user_coin5->address = 'Ethudsdjhdjhdhjhhrhejhdjhje';
+//        $user_coin5->coin_id = 5;
+//        $user_coin5->amount = 10;
+//        $user_coin5->earn = 2;
+//        $user_coin5->save();
     }
 
 }
